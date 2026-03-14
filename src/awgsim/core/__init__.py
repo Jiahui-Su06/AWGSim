a77dc322f1e700a5326bb476a6028ffd016f9f48
Jia-Hui Su <sjh855210@qq.com>
2026-03-15 00:54:11 +0800

    core V2

--- src/awgsim/core/__init__.py
+++ src/awgsim/core/__init__.py
@@ -1,5 +1,17 @@
 from .clamp import clamp
 from .rect  import rect
+from .step import step
 from .pnorm import pnorm
+from .fpower import fpower
+from .gmode import gmode
+from .overlap import overlap
+from .diffract import diffract
+from .slabindex import slabindex
+from .slabmode import slabmode
+from .wgindex import wgindex
+from .wgmode import wgmode
 
 
+__all__ = ["clamp", "rect", "pnorm", "fpower", "gmode", 
+           "overlap", "step", "diffract", "slabindex", 
+           "slabmode", "wgindex", "wgmode"]
